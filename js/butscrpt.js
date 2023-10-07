6b15a0132fa62baaeb030abb0cb93462c7e73f6d
Batraz <batraz.tolasovv@gmail.com>
2023-10-07 21:24:10 +0300

    beta

---
+++ js/butscrpt.js
@@ -0,0 +1,5 @@
+$(function(){
+    $('.personal-account_profile_form_lable_gender-button').on('click', function(){
+        $('.personal-account_profile_form_lable_gender-button').toggleClass('personal-account_profile_form_lable_gender-button--active')
+    });
+});
